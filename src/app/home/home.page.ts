a822074586cd8423eb55bb1150d217ecac46e9b8
GaryVillegas <gary.villegasb@gmail.com>
2025-05-29 18:08:54 -0400

    TODO: terminar conseguir info usuario home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { getUserStoreData } from '../types/store';
 import { StoreService } from '../store.service';
+import { AuthService } from '../auth.service';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -10,12 +12,27 @@ import { StoreService } from '../store.service';
 })
 export class HomePage implements OnInit {
   stores: getUserStoreData[] = [];
-  constructor(private storeServ: StoreService) {}
+  constructor(
+    private storeServ: StoreService,
+    private toastController: ToastController,
+    private authServ: AuthService
+  ) {}
 
   ngOnInit() {
     this.storeServ.getAllStores().subscribe((stores) => {
       this.stores = stores;
       console.log('Todas las Tiendas', stores);
     });
+  }
+
+  async presentToast(header: string, message: string, color: string) {
+    const toast = await this.toastController.create({
+      header: header,
+      message: message,
+      duration: 2000,
+      position: 'top',
+      color: color,
+    });
+    toast.present();
   }
 }
