250ad42d2f705d0c82de33b351b87927a5b85e49
GaryVillegas <gary.villegasb@gmail.com>
2025-05-31 14:51:59 -0400

    FEAT [home]: Se trata de mostrar las tiendas y sus servicios por parte de un modal

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -43,44 +43,6 @@ export class HomePage implements OnInit {
     return this.stores.find((store) => store.documentId === this.storeSelected);
   }
 
-  //Modal Service Store
-  isModalStoreService = true;
-  storeId: string | null = null;
-  services: ServiceData[] = [];
-  setModalStoreService(isOpen: boolean, storeId: string) {
-    this.isModalStoreService = isOpen;
-    this.storeId = storeId;
-    this.getServiceStore(storeId);
-    console.log('ID de la tienda seleccionada:', storeId);
-    console.log('Modal de servicio abierto:', isOpen);
-  }
-
-  async getServiceStore(storeId: string) {
-    try {
-      this.isLoading = true; // Mostrar indicador de carga
-      const services = await this.storeServ
-        .getServicesByStoreId(storeId)
-        .toPromise();
-      console.log('Servicios de la tienda:', services);
-
-      // Asignar servicios o array vacío
-      this.services = services || [];
-
-      this.isLoading = false; // Ocultar indicador de carga
-      return services;
-    } catch (error) {
-      this.isLoading = false; // Asegurarse de ocultar el indicador de carga en caso de error
-      console.error('Error al obtener los servicios de la tienda:', error);
-      this.presentToast(
-        'Error',
-        'No se pudieron cargar los servicios de la tienda.',
-        'danger'
-      );
-      this.services = []; // Asignar array vacío en caso de error
-      return [];
-    }
-  }
-
   async presentToast(header: string, message: string, color: string) {
     const toast = await this.toastController.create({
       header: header,
