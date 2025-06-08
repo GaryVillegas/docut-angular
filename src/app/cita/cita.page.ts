465fe483f85d48268b373119365270170ad67511
GaryVillegas <gary.villegasb@gmail.com>
2025-06-08 01:08:04 -0400

    feat[cita]: se puede ver el calendario.

--- src/app/cita/cita.page.ts
+++ src/app/cita/cita.page.ts
@@ -14,6 +14,7 @@ import { tap } from 'rxjs';
 export class CitaPage implements OnInit {
   serviceId: string | null = null;
   serviceData: ServiceData | null = null;
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private storeServ: StoreService,
@@ -25,21 +26,38 @@ export class CitaPage implements OnInit {
     if (!this.serviceId) {
       this.presentToast('Error', 'No hay un servicio seleccionado.', 'danger');
     } else {
-      this.storeServ
-        .getServiceById(this.serviceId)
-        .pipe(
-          tap((data) => {
-            this.serviceData = data;
-            console.log('Service Data: ', data);
-          })
-          /**\
-           * tap: Para efectos secundarios (console.log, asignaciones, llamadas a funciones que no retornan datos)
-           */
-        )
-        .subscribe();
+      this.loadServiceData(this.serviceId);
     }
+    this.minDate = new Date().toISOString();
   }
 
+  //cargar los datos del servicio
+  loadServiceData(id: string) {
+    this.storeServ
+      .getServiceById(id)
+      .pipe(
+        tap((data) => {
+          this.serviceData = data;
+          console.log('Service Data: ', data);
+        })
+        /**\
+         * tap: Para efectos secundarios (console.log, asignaciones, llamadas a funciones que no retornan datos)
+         */
+      )
+      .subscribe();
+  }
+
+  //algoritmo de seleccion
+  selectedDate: string = '';
+  minDate: string = '';
+
+  //Actualizar fecha reciente
+  onDateChange(event: any) {
+    this.selectedDate = event.detail.value;
+    console.log('Fecha seleccionada:', this.selectedDate);
+  }
+
+  //toas
   async presentToast(header: string, message: string, color: string) {
     const toast = await this.toastController.create({
       header: header,
