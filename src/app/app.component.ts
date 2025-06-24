21784e18d1b70f0bd19cd7107f36b4d3cd0b5c25
GaryVillegas <gary.villegasb@gmail.com>
2025-06-24 11:21:14 -0400

    FEAT[notifications]: start with notifiactions configurations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
+import {
+  ActionPerformed,
+  PushNotificationSchema,
+  PushNotifications,
+  Token,
+} from '@capacitor/push-notifications';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +16,13 @@ import { Router } from '@angular/router';
   standalone: false,
 })
 export class AppComponent implements OnInit {
-  constructor(private auth: AngularFireAuth, private router: Router) {}
+  constructor(
+    private auth: AngularFireAuth,
+    private router: Router,
+    private platform: Platform
+  ) {
+    if (this.platform.is('capacitor')) this.initPush();
+  }
 
   ngOnInit() {
     this.auth.authState.subscribe((user) => {
@@ -22,5 +35,45 @@ export class AppComponent implements OnInit {
         this.router.navigate(['/login']); // Navigate to login page
       }
     });
+  }
+
+  //FIXME: terminar notificaciones despues de terminar los demas modulos.
+  initPush() {
+    console.log('Initializing HomePage');
+
+    //Request permission to use push notifications
+    //IOS will prompt user and retuirn if they granted permission or not
+    //Android will just grant without propting
+    PushNotifications.requestPermissions().then((result) => {
+      if (result.receive === 'granted') {
+        PushNotifications.register();
+      } else {
+        //Show some error
+      }
+    });
+
+    //On success, we should be able to recive notifications
+    PushNotifications.addListener('registration', (token: Token) => {
+      alert('Push registration success, toke: ' + token.value);
+    });
+
+    //Some issue with our setup and push will not work
+    PushNotifications.addListener('registrationError', (error: any) => {
+      alert('Error on registration: ' + JSON.stringify(error));
+    });
+
+    PushNotifications.addListener(
+      'pushNotificationReceived',
+      (notification: PushNotificationSchema) => {
+        alert('Push received: ' + JSON.stringify(notification));
+      }
+    );
+
+    PushNotifications.addListener(
+      'pushNotificationActionPerformed',
+      (notification: ActionPerformed) => {
+        alert('Push action performed: ' + JSON.stringify(notification));
+      }
+    );
   }
 }
